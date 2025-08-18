95e2711bcc74fb354cfd34e12b420ed3933f5713
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-18 11:07:16 +0900

    add how to learn loop in loop

--- besicPython.py
+++ besicPython.py
@@ -1,6 +1,10 @@
-count = 0
-for i in range(1,6):
-    number = input("No."+str(i)+":")
-    print(number)
+#九九
+start = int(input("最初の九九:"))
+end = int(input("最後の九九:"))
 
-print("Hello world")
+for number in range(start,end+1):
+    print("九九",number)
+    print("----------")
+    for i in range(1,13):
+        print(number,"x",i,"=",number*i)
+    print("--------")
