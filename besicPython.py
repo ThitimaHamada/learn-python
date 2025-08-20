e548b3bd8314f894685d2113b176ae3f0f28b6c4
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-20 11:29:20 +0900

    learn literal Pattern

--- besicPython.py
+++ besicPython.py
@@ -1,1 +1,11 @@
-
+#literal Pattern
+service = 10
+match service:
+    case 1 : 
+        print("預入")
+    case 2 : 
+        print("引き出し")
+    case 3 : 
+        print("残高")
+    case None:
+        print("情報が間違っている")
