b7b684b6b7c11d3470fcbeaa1c1c4b7d22b0e97a
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-20 15:04:49 +0900

    learn mapping pattern

--- besicPython.py
+++ besicPython.py
@@ -1,9 +1,18 @@
-#Sequence pattern
-data = [1,2]
-match data:
-    case []:
-        print("No infornamation")
-    case [1,2]:
-        print("information are 1 and 2")
-    case [1,2,3]:
-        print("information are 1,2 and 3")
+#Mapping Pattern
+customers = [
+    {"name":"Tawan","type":"general"},
+    {"name":"Jan","type":"member"},
+    {"name":"Nan","type":"general"}
+
+]
+id = int(input("Enter your id:"))
+print(f"Hello,your id is {id} : {customers[id]["name"]}")
+
+match customers[id]:
+    case {"type":"member"}:
+        print("You are member 50% ")
+    case _:
+        print("No discount")
+
+
+
