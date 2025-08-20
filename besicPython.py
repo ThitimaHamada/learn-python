0e849f7eb1b8510febb637488aad88cc1817096b
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-20 15:47:50 +0900

    learn def function

--- besicPython.py
+++ besicPython.py
@@ -1,18 +1,25 @@
-#Mapping Pattern
-customers = [
-    {"name":"Tawan","type":"general"},
-    {"name":"Jan","type":"member"},
-    {"name":"Nan","type":"general"}
+#Create Function
+def sayHello(time,username,age): #parameter
+    print("Hello",time,username)
+    print("This year you are",age,"years old")
 
-]
-id = int(input("Enter your id:"))
-print(f"Hello,your id is {id} : {customers[id]["name"]}")
+def saveEmployee(name,departmemt,salary):
+    print(f"Name {name},Department {departmemt}")
+    print(f"Salary {salary} yen")
 
-match customers[id]:
-    case {"type":"member"}:
-        print("You are member 50% ")
-    case _:
-        print("No discount")
+def showTable(num):
+    print("---------")
+    for i in range(1,13):
+        print(f"{num} x {i} = {num*i}")
+
+#mytime = "morning"
+#sayHello(mytime,"Tawan",31) #argument
+#sayHello(mytime,"Jojo",25)
+#showTable(3)
+#showTable(5)
+saveEmployee("Tawan","IT",700000)
+saveEmployee("Nut","graphic",800000)
+saveEmployee("Jo","sale",600000)
 
 
 
