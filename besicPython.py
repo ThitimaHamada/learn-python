3a44fd312864e77ad340a25922fb3faf7d634d8f
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-22 10:35:22 +0900

    add return function

--- besicPython.py
+++ besicPython.py
@@ -1,15 +1,15 @@
-#arge / kwargs
+#return function
+def getCapital():
+    return "Tokyo"
 
-def saveEmployee(**kwargs): #tuple
-    print(f"Name {kwargs["name"]},Department {kwargs["department"]}")
-    print(f"Salary {kwargs["salary"]} yen")
-    print(f"Address {kwargs["address"]} ")
-    print("----------")
+def getPI():
+    return 3.14
+
+radius = 5
+area = getPI()*radius**2
+print("Cicle area =",area)
 
 
-saveEmployee(name="Tawan",department="IT",salary=700000,address="Ueno")
-saveEmployee(name="Nut",department="graphic",salary=800000,address="Tokyo")
-saveEmployee(name="Jo",department="sale",salary=600000,address="Kyoto")
 
 
 
