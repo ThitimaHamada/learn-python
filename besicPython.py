8d7b4b26734ec22295dfc5646827bd79a0cb18c8
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-22 11:34:16 +0900

    add variable scorp

--- besicPython.py
+++ besicPython.py
@@ -1,7 +1,21 @@
-#lambda function
-result = lambda base,n : base**n
-print("Result =",result(2,3))
-print("Result =",result(5,3))
+#local variable / global variable/variable scorp
 
+balance = 1000 #global
+def displayBalance():
+    print("Account balance =",balance,"yen")
 
+def deposit(value):
+    global balance
+    money =value #local 
+    print("deposit =",money,"yen")
+    balance+=money
 
+def withdraw(value):
+    global balance
+    amount = value
+    print("withdraw =",amount,"yen")
+    balance-=amount
+
+displayBalance()
+withdraw(500)
+displayBalance()
