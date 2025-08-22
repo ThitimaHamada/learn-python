ea5d4851b81f4ccbb7759ce83caad2c6954a7ef8
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-22 10:49:29 +0900

    add more return function

--- besicPython.py
+++ besicPython.py
@@ -1,14 +1,21 @@
-#return function
-def getCapital():
-    return "Tokyo"
+#para + return function
+def checkNumber(number):
+    if number%2==0:
+        return "ku"
+    else:
+        return "ki"
 
-def getPI():
-    return 3.14
+def summation(*data):
+    total = 0
+    for item in data:
+        total+=item
+    return total
 
-radius = 5
-area = getPI()*radius**2
-print("Cicle area =",area)
 
+#result = checkNumber(13)
+#print("Result =",result)
+print(summation(10,20))
+print(summation(10,20,30))
 
 
 
