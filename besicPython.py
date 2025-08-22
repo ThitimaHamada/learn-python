65c341e2b9d12eecdffc8756ebbdc1726a82b3b9
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-22 11:11:47 +0900

    lambda function

--- besicPython.py
+++ besicPython.py
@@ -1,22 +1,7 @@
-#para + return function
-def checkNumber(number):
-    if number%2==0:
-        return "ku"
-    else:
-        return "ki"
-
-def summation(*data):
-    total = 0
-    for item in data:
-        total+=item
-    return total
-
-
-#result = checkNumber(13)
-#print("Result =",result)
-print(summation(10,20))
-print(summation(10,20,30))
-
+#lambda function
+result = lambda base,n : base**n
+print("Result =",result(2,3))
+print("Result =",result(5,3))
 
 
 
