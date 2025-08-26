99872968c1c197e6c6d9a841e25ee26ad70c061d
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-26 11:47:40 +0900

    add return line to solve bug

--- besicPython.py
+++ besicPython.py
@@ -8,14 +8,22 @@ def deposit(value):
     global balance
     money =value #local 
     print("deposit =",money,"yen")
+    if (money<=0):
+        print("you can not do deposit")
+        return
+    
     balance+=money
 
 def withdraw(value):
     global balance
     amount = value
     print("withdraw =",amount,"yen")
+    if amount<=0 or amount>balance:
+        print("you can not do withdraw")
+        return
+    
     balance-=amount
 
 displayBalance()
-withdraw(500)
+withdraw(1200)
 displayBalance()
