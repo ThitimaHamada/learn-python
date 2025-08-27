90e650eef5a68d3a6d89cd39b09855ca148a6e8e
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-27 11:11:03 +0900

    add raise exception

--- besicPython.py
+++ besicPython.py
@@ -2,6 +2,8 @@
 try:
     number1 = int(input("Enter number No.1:"))
     number2 = int(input("Enter number No.2:"))
+    if number1<0 or number2<0:
+        raise Exception("The numeric data must be positive only!")
     result = number1/number2
     print("Result = ",result)
 
