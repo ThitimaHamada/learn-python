e25fe040b39016ec28711b00cf2fd2a05d2c9823
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-27 11:00:39 +0900

    learn how to use Exception

--- besicPython.py
+++ besicPython.py
@@ -1,29 +1,15 @@
-#local variable / global variable/variable scorp
+#Excetion
+try:
+    number1 = int(input("Enter number No.1:"))
+    number2 = int(input("Enter number No.2:"))
+    result = number1/number2
+    print("Result = ",result)
 
-balance = 1000 #global
-def displayBalance():
-    print("Account balance =",balance,"yen")
-
-def deposit(value):
-    global balance
-    money =value #local 
-    print("deposit =",money,"yen")
-    if (money<=0):
-        print("you can not do deposit")
-        return
-    
-    balance+=money
-
-def withdraw(value):
-    global balance
-    amount = value
-    print("withdraw =",amount,"yen")
-    if amount<=0 or amount>balance:
-        print("you can not do withdraw")
-        return
-    
-    balance-=amount
-
-displayBalance()
-withdraw(1200)
-displayBalance()
+except ValueError:
+    print("The information incorrect,Please enter only number")
+except ZeroDivisionError:
+    print("Cannot be divided by zero")
+finally:
+    print("---------")
+    print("End program")
+    print("---------")
