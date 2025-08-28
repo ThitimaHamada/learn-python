c109807768b3d408c9c0e593162a52aeb70536db
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-28 12:06:45 +0900

    learn numpy how to make array

--- besicPython.py
+++ besicPython.py
@@ -1,17 +1,8 @@
-#Excetion
-try:
-    number1 = int(input("Enter number No.1:"))
-    number2 = int(input("Enter number No.2:"))
-    if number1<0 or number2<0:
-        raise Exception("The numeric data must be positive only!")
-    result = number1/number2
-    print("Result = ",result)
-
-except ValueError:
-    print("The information incorrect,Please enter only number")
-except ZeroDivisionError:
-    print("Cannot be divided by zero")
-finally:
-    print("---------")
-    print("End program")
-    print("---------")
+#Numpy
+import numpy as np
+arr=np.array(1)
+arr=np.array(2)
+a=np.array([[1,2],[3,4,],[5,6]])
+b=np.array([1,2,3,4,5])
+b[3]=100
+print(a[2][0])
