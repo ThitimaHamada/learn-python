0623ae7d31de3a574d42968c25a807bfd3c7516c
Thitima Hamada <tavan.vn7@gmail.com>
2025-08-29 10:53:33 +0900

    numpy 3d array

--- besicPython.py
+++ besicPython.py
@@ -2,7 +2,7 @@
 import numpy as np
 arr=np.array(1)
 arr=np.array(2)
-a=np.array([[1,2],[3,4,],[5,6]])
-b=np.array([1,2,3,4,5])
-b[3]=100
-print(a[2][0])
+a=np.array([[[1,2],[3,4,],[5,6]]])
+b=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+
+print(b[0][0][2])
